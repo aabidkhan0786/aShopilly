94d31ad078457664f46972397a8e345f3cad6880
aabidkhan0786 <khanaab786@gmail.com>
2021-09-05 22:11:21 +0530

    navbar chnage !!

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,41 +1,50 @@
 import Link from 'next/link'
-
+import { useRouter } from 'next/router'
 const NavBar = () => {
-    return (
-        <>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container-fluid">
-        <a className="navbar-brand h2" href="/">aShoppilly</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-    
-        <div className="collapse navbar-collapse" id="navbarColor01">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link href="/login">             
-              <a className="nav-link active" >LogIn</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/signup">             
-              <a className="nav-link active" >SignUp</a>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link href="/create">             
-              <a className="nav-link active" >Create</a>
-              </Link>
-            </li>
+  const router = useRouter();
 
-    
-          </ul>
-    
+  const active_nav = (route) => {
+    if (route == router.pathname) {
+      return "active bg-danger  nav-item"
+    } else {
+      return "nav-item"
+    }
+  }
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand h2" href="/">aShoppilly</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav ml-auto">
+              <li className={active_nav("/login")}>
+                <Link href="/login">
+                  <a className="nav-link" >LogIn</a>
+                </Link>
+              </li>
+              <li className={active_nav("/signup")}>
+                <Link href="/signup">
+                  <a className="nav-link " >SignUp</a>
+                </Link>
+              </li>
+              <li className={active_nav("/create")}>
+                <Link href="/create">
+                  <a className="nav-link " >Create</a>
+                </Link>
+              </li>
+
+
+            </ul>
+
+          </div>
         </div>
-      </div>
-    </nav>
-        </>
-    )
+      </nav>
+    </>
+  )
 }
 
 export default NavBar
--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,18 @@ import baseUrl from "../database/baseUrl";
 export default function Home({ products }) {
   return (
     <>
-      <div className="row-fluid ">
+      {/* <div className="row-fluid ">
         <div className="col-12 d-flex bd-highlight">
-          {/* style={{"max-width": "18rem"}} */}
+          {/* style={{"max-width": "18rem"}} 
           {
             products.map((prod) => (
               <>
-
                 <div className="col-12 col-md-3 mx-auto m-2 ">
                   <div className="card shadow-lg mb-5 bg-white rounded" >
                     <img src={prod.mediaUrl} className="card-img-top img-fluid" alt={prod.name} />
                     <div className="card-body">
                       <h5 className="card-title h3">{prod.name}- <span className="float-right"><b>{prod.price} Rs</b></span></h5>
-                      {/* <h5 className="card-title"></h5> */}
+                      {/* <h5 className="card-title"></h5> 
                       <p className="card-text h5">{prod.description}</p>
                       <center>
                         <Link href={"/product/[id]"} as={`/product/${prod._id}`} >
@@ -32,7 +31,7 @@ export default function Home({ products }) {
             ))
           }
         </div>
-      </div>
+      </div> */}
     </>
   )
 }
